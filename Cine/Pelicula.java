1216306b349aa63bb928fabd6a786f82c646ceb2
Victor <victorpinero21@gmail.com>
2024-03-24 00:13:12 +0100

    Borramos un private String

--- Cine/Pelicula.java
+++ Cine/Pelicula.java
@@ -4,7 +4,7 @@ public class Pelicula {
 	private int year;
 	private String director;
 	private String genero;
-	private String nombre;
+	
 	
 
 	public Pelicula(int year, String director, String genero, String nombre) {
